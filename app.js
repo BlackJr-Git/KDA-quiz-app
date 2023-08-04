ccad9babe3aacd58acc9d6562b5e9ce0f5d2c480
BlackJr-Git <juniorassosa@gmail.com>
2023-08-04 17:26:26 +0200

    feat: question fill on quiz form in js and switch form

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const quizForm = document.getElementById("Quiz-form");
 const btnQuiz = document.getElementById("btn-quiz");
 
+const btnStartQuiz = document.getElementById("start-quiz");
+const registrationForm = document.getElementById("reg-form");
+
+const questionLabel = document.getElementById("question");
+const firstAnswer = document.getElementById("answer1");
+const secondAnswer = document.getElementById("answer2");
+const thirdAnswer = document.getElementById("answer3");
+const fourthAnswer = document.getElementById("answer4");
+
 const player = {
   name: "player",
   mail: "player@quiz.app",
@@ -8,15 +17,31 @@ const player = {
 };
 
 const question1 = {
-    question : "Quel est le type d'un fichier javascript ?",
+  question: "Quel est le type d'un fichier javascript ?",
 
-    trueAnswer : ".js",
-    answer1 : '.jsx',
-    answer2 : '.j',
-    answer3 : '.ts'
+  trueAnswer: ".js",
+  answer1: ".jsx",
+  answer2: ".j",
+  answer3: ".ts",
+};
+
+function remplir() {
+  questionLabel.innerText = question1.question;
+
+  firstAnswer.innerText = question1.answer1;
+  secondAnswer.innerText = question1.answer2;
+  thirdAnswer.innerText = question1.trueAnswer;
+  fourthAnswer.innerText = question1.answer3;
+
+  registrationForm.style.display = "none";
+  console.log('Bonjour')
 }
 
+// btnStartQuiz.onclick = function() {
+//     console.log(Bonjour)
+// }
 
+btnStartQuiz.addEventListener("click", remplir);
 
 // Switch du entre la page connexion et la page question
 
@@ -25,9 +50,10 @@ const question1 = {
 // };
 
 function displayQuizForm() {
-//   quizForm.style.display = "";
-  quizForm.style.removeProperty("display");
-//   console.log(quizForm.style);
+  //   quizForm.style.display = "";
+  //   quizForm.style.removeProperty("display");
+  //   console.log(quizForm.style);
+  quizForm.style.zIndex = "11";
 }
 
-btnQuiz.addEventListener("click", displayQuizForm);
+// btnQuiz.addEventListener("click", displayQuizForm);
