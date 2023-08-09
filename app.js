c60bdc556b299918c8e777e8ace3a80ddd3b86ba
BlackJr-Git <juniorassosa@gmail.com>
2023-08-09 12:03:25 +0200

    add : gitInputValue getter and score form

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
+// differentes pages du quiz
 const quizForm = document.getElementById("Quiz-form");
-const btnQuiz = document.getElementById("btn-quiz");
+const registrationForm = document.getElementById("reg-form");
 
+// timer et compte a rebours
 const timer = document.getElementById("progress");
+const countdownText = document.getElementById("countdown");
+const progressBar = document.querySelector(".progress div");
 
 // Name and Email Check
-
 const nameInput = document.getElementById("name-input");
 const mailInput = document.getElementById("mail-input");
 const nameRequired = document.getElementById("name-input-required");
 const mailRequired = document.getElementById("mail-input-required");
-// const getName = nameInput.value;
-// const getMail = mailInput.value;
 
+// bouttons
 const btnStartQuiz = document.getElementById("start-quiz");
-const registrationForm = document.getElementById("reg-form");
+const btnValid = document.getElementById("btnvalid");
 
+// labels Question et reponses
 const questionLabel = document.getElementById("question");
 const firstAnswer = document.getElementById("answer1");
 const secondAnswer = document.getElementById("answer2");
 const thirdAnswer = document.getElementById("answer3");
 const fourthAnswer = document.getElementById("answer4");
 
-const btnValid = document.getElementById("btnvalid");
-
+// compte de question
 const questionCount = document.getElementById("question-count");
-const countdownText = document.getElementById("countdown");
 
-const progressBar = document.querySelector(".progress div");
-
-// const checkedInput = document.getElementsByTagName("input");
-
+// Cette section Contient les Objets : Player et Question
 const player = {
   name: "player",
   mail: "player@quiz.app",
@@ -48,24 +46,41 @@ const question1 = {
 const question2 = {
   question: "Quel est la langue officiel en rdc?",
 
-  trueAnswer: "Allemand",
+  trueAnswer: "Francais",
   answer1: "Anglais",
   answer2: "Espagnol",
-  answer3: "Francais",
+  answer3: "Allemand",
 };
 
 const question3 = {
   question: "Quel est le plus grand antagoniste d'un manga?",
 
-  trueAnswer: "Madara",
-  answer1: "Meruem",
+  trueAnswer: "Meruem",
+  answer1: "Madara",
   answer2: "Dio Brando",
   answer3: "Sneizel",
 };
 
-// Gestion des objects question
+const question4 = {
+  question: "Quel est le plus grand pays du monde?",
 
-const questionArrays = [question1, question2, question3]; // Liste de question
+  trueAnswer: "Russie",
+  answer1: "Chine",
+  answer2: "Canada",
+  answer3: "Mongolie",
+};
+
+const question5 = {
+  question: "Quel est la langue la plus parler du monde?",
+
+  trueAnswer: "Chinois",
+  answer1: "Francais",
+  answer2: "Anglais",
+  answer3: "Portugais",
+};
+
+// Operation pour rendre les question aleatoire lors du lancement du quiz
+const questionArrays = [question1, question2, question3, question5, question4]; // Liste de question
 let randQuestion = Math.floor(Math.random() * questionArrays.length);
 let displayedQuestion = [];
 
@@ -77,6 +92,7 @@ function takeRandomQuestion() {
   }
 }
 
+// Fonction qui permet de remplir les champs dynamique des questions
 function remplir() {
   let randQuestion = Math.floor(Math.random() * questionArrays.length);
   // Quiz form fill
@@ -91,12 +107,22 @@ function remplir() {
     thirdAnswer.innerText = questionArrays[randQuestion].trueAnswer;
     fourthAnswer.innerText = questionArrays[randQuestion].answer3;
 
+    console.log(Input1.value, Input2.value, Input3.value, Input4.value);
+
     displayedQuestion.push(randQuestion);
   }
 
   registrationForm.style.display = "none";
 }
 
+function getInputValue() {
+  Input1.value = questionArrays[randQuestion].answer1;
+  Input2.value = questionArrays[randQuestion].answer2;
+  Input3.value = questionArrays[randQuestion].trueAnswer;
+  Input4.value = questionArrays[randQuestion].answer3;
+}
+
+// Fonction qui permet l'enregistrement du joueur
 function registerPlayerId(event) {
   event.preventDefault();
   if (nameInput.value == "" && mailInput.value == "") {
@@ -111,16 +137,15 @@ function registerPlayerId(event) {
     player.mail = mailInput;
 
     remplir();
-    // console.log(getName, getMail);
     console.log(nameInput.value, mailInput.value);
   }
 }
 
-btnValid.addEventListener("click", remplir);
+// Ecouteur d'evenement
+
 btnStartQuiz.addEventListener("click", registerPlayerId);
 
 // Gestion du timer
-
 let timeLeft = 10;
 let countNumQuestion = 0;
 
@@ -137,13 +162,13 @@ function startTimer() {
     } else {
       // clearInterval(timerInterval);
       countNumQuestion++;
-      console.log(countNumQuestion)
+      console.log(countNumQuestion);
       if (countNumQuestion <= 5) {
         timeLeft = 10;
         remplir();
         questionCount.innerText++;
         console.log(questionCount.innerText);
-        uncheckInputRadio()
+        uncheckInputRadio();
       } else {
         clearInterval(timerInterval);
       }
@@ -153,59 +178,82 @@ function startTimer() {
 
 function resetTimer() {
   countNumQuestion++;
+  
   if (countNumQuestion <= 5) {
+    getInputValue();
+    getCheckedValue();
     timeLeft = 10;
     timeLeft--;
     updateProgressBar();
     countdownText.innerText = timeLeft;
     questionCount.innerText++;
-    uncheckInputRadio()
+    uncheckInputRadio();
   } else {
     clearInterval(timerInterval);
   }
 }
 
 btnStartQuiz.addEventListener("click", startTimer);
+btnValid.addEventListener("click", remplir);
 btnValid.addEventListener("click", resetTimer);
 
-// btnStartQuiz.addEventListener("click", remplir);
 // radio input variables get
-  const Input1 = document.getElementById("ans1");
-  const Input2 = document.getElementById("ans2");
-  const Input3 = document.getElementById("ans3");
-  const Input4 = document.getElementById("ans4");
+const Input1 = document.getElementById("ans1");
+const Input2 = document.getElementById("ans2");
+const Input3 = document.getElementById("ans3");
+const Input4 = document.getElementById("ans4");
 
- 
+// Fonction qui desactive le boutton valider si les inputs ne sont pas cocher
 function disableBtnValider() {
   if (Input1.checked || Input2.checked || Input3.checked || Input4.checked) {
     btnValid.removeAttribute("disabled");
   } else {
-
     btnValid.setAttribute("disabled", "true");
   }
 }
 
-// radioninput Event 
-  Input1.addEventListener("change", disableBtnValider);
-  Input2.addEventListener("change", disableBtnValider);
-  Input3.addEventListener("change", disableBtnValider);
-  Input4.addEventListener("change", disableBtnValider);
+// radioninput Event
+Input1.addEventListener("change", disableBtnValider);
+Input2.addEventListener("change", disableBtnValider);
+Input3.addEventListener("change", disableBtnValider);
+Input4.addEventListener("change", disableBtnValider);
 
-
-
-function uncheckInputRadio(){
+// Fonction qui permet de reinitialiser l'etat des inputs apres la validation
+function uncheckInputRadio() {
   Input1.checked = false;
   Input2.checked = false;
   Input3.checked = false;
   Input4.checked = false;
   btnValid.setAttribute("disabled", "true");
 }
-// setInterval(() => {
-//     if (checkedInput.checked){
-//         btnValid.removeAttribute("disabled")
-//     } else {
-//         btnValid.setAttribute("disabled", "true")
-//     }
-// }, 100);
 
+// Fonction qui recupere la valeurs de l'input cheched
+// function getCheckedValue() {
+//   const radioInputs = document.querySelectorAll('input[type="radio"]');
+//   let valeurCochee = null;
+
+//   radioInputs.forEach(function(input) {
+//       if (input.checked) {
+//           valeurCochee = input.value;
+//           console.log("La valeur cochée est :",input.value);
+//       }
+//   });
+// }
+
+function getCheckedValue() {
+  let answerChecked;
+  if (Input1.checked === true) {
+    answerChecked = Input1.value;
+  } else if (Input2.checked === true) {
+    answerChecked = Input2.value;
+  } else if (Input3.checked === true) {
+    answerChecked = Input3.value;
+  } else if (Input4.checked === true) {
+    answerChecked = Input4.value;
+  }
+
+  console.log(`la valeur selectionner est :  ${answerChecked}`);
+}
+
+btnValid.addEventListener("click", getCheckedValue);
 console.log(nameInput.value, mailInput.value);
